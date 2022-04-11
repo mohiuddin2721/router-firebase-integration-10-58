fbe32f047fe1e122727c103c440a8a7b64b34803
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-04-11 22:35:28 +0600

    implement auth redirect

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,19 +1,32 @@
 import { getAuth } from 'firebase/auth';
 import React from 'react';
 import app from '../../firebase.init';
-import {useSignInWithGoogle} from 'react-firebase-hooks/auth';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useLocation, useNavigate } from 'react-router-dom';
 // import useFirebase from '../../hooks/useFirebase';
 
 const auth = getAuth(app);
 
+
 const Login = () => {
     // const {singInWithGoogle} = useFirebase();
     const [signInWithGoogle, user] = useSignInWithGoogle(auth);
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const from = location?.state?.from?.pathname || "/";
+
+    const handleGoogleSignIn = () => {
+        signInWithGoogle()
+        .then( () => {
+            navigate(from, { replace: true });
+        })
+    }
     return (
         <div>
             <h3>This is login</h3>
-            <div style={{margin: '20px'}}>
-                <button onClick={ () => signInWithGoogle()}>Google Sign In</button>
+            <div style={{ margin: '20px' }}>
+                <button onClick={handleGoogleSignIn}>Google Sign In</button>
             </div>
             <form>
                 <input type="email" placeholder='Your Email' />
